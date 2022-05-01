c9d5437b8101c288f1434c37277d0bbc91889dda
Wes Anderson <wesley.herb.anderson@gmail.com>
2022-05-01 12:34:39 -0400

    adding prod domain name and removing need for https

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
+// dev ingress: http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+// prod ingress: http://gittix.org
+
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://gittix.org',
       headers: req.headers
     });
   } else {
